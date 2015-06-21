04e18949583967f184a3aec31f267e31ee78230c
ryandkerr <ryankerr@college.harvard.edu>
2015-06-21 13:55:09 -0400

    get_classes now gets appropriate absolute links to class pages for a given department link

--- q-scraper2.py
+++ q-scraper2.py
@@ -35,62 +35,46 @@ SESSION_ID = '6EA803C3E5E3B46999FA7A69CF2D3EEA'
 Q_ROOT = 'https://webapps.fas.harvard.edu/course_evaluation_reports/fas/list?yearterm=2014_1'
 
 # helper for making soup using cookies
-def make_soup(page_url): 
+def make_soup(section_url): 
     session = requests.Session()
     session.cookies = requests.utils.cookiejar_from_dict({
         'JSESSIONID': SESSION_ID
     })
     session.verify = False
-     
-    request = session.get(page_url)
+    request = session.get(section_url)
     return BeautifulSoup(request.text, "lxml")
  
 # takes base url and returns array of department drop-down pages
 def get_class_links(section_url):
     soup = make_soup(section_url)
-
-    # depts = soup.find_all("div", "course-block-head")
-    # print(depts)
-
     triangles = soup.find_all("a", "remove_link")
-    
-    # print(triangles)
-
     link_list = []
     for triangle in triangles:
         link = triangle.get("href")
         l = Q_ROOT + "&" + link.split("?")[-1]
         if not l in link_list:
             link_list.append(l)
-    # link_list = []
-    # for dept in depts:
-    #     d = dept.find("span")
-        # print(d)
-        # base = "https://webapps.fas.harvard.edu/course_evaluation_reports/fas/list?dept="
-        # link_list.append(base + d)
-
     return link_list
 
-# def get_classes(section_url):
-#     soup = make_soup(section_url)
-#     selected_dept = section_url.split("#")[-1]
-    
-#     depts = soup.find_all("div", "course-block")
+def get_classes(section_url):
+    soup = make_soup(section_url)
+    selected_dept = section_url.split("#")[-1]
+    depts = soup.find_all("div", "course-block")
+    classes = []
+    for dept in depts:
+        if dept.find("span", "course-block-title")["title"] == selected_dept:
+            # now dept is the course-block of our department
+            courses = dept.find_all("li", "course")
+            for course in courses:
+                c = "https://webapps.fas.harvard.edu/course_evaluation_reports/fas/" + course.a["href"]
+                classes.append(c)
 
-#     classes = []
-#     for dept in depts:
-#         if dept.find("span", "course-block-title")["title"] == selected_dept:
-
-#             # now dept is the course-block of our department
-#             courses = dept.find_all("li", "course")
-
-#             for course in courses:
-#                 classes.append(course.a["href"])
-
-#     return classes
+    return classes
 
 # all_classes = []
 
 class_links = get_class_links(Q_ROOT)
 
-print(class_links)
+dept_classes = get_classes(class_links[0])
+
+print(dept_classes)
