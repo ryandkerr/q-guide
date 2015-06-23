037d88e52809b7daaeb9efcaf2cee43d320aba36
ryandkerr <ryankerr@college.harvard.edu>
2015-06-22 22:26:26 -0400

    testing regex and splitting for all words in a row

--- counter.py
+++ counter.py
@@ -3,9 +3,22 @@
 # reads q-guide csv and counts most-used words
 
 import csv
+import re
 from collections import Counter
 
 with open("data/test_q.csv", "rb") as test_csv:
     test = csv.reader(test_csv)
     for row in test:
-        print row[3]
+        if row[0] == "APCOMP 209: Data Science":
+            all_words = []
+            for i, review in enumerate(row):
+                words = re.findall(r'\w+', review)
+                all_words += words
+
+
+            print all_words
+
+
+
+            # c = Counter(row)
+            # print c
