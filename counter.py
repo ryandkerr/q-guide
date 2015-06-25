2f87aae8ce590fbc7dee40e70c4363d6e49f0d15
ryandkerr <ryankerr@college.harvard.edu>
2015-06-25 00:06:17 -0400

    test.html with percent-adjusted for one class, looks promising. new data file output

--- counter.py
+++ counter.py
@@ -12,14 +12,15 @@ from collections import Counter
 def export_test():
     with open("data/test_q.csv", "rb") as test_csv:
         test = csv.reader(test_csv)
+        all_words = []
         for row in test:
             if row[0] == "APCOMP 209: Data Science":
-                all_words = []
                 for i, review in enumerate(row):
                     words = re.findall(r'\w+', review)
                     all_words += words
-                lower_words = [word.lower() for word in all_words]
-                c = Counter(lower_words)
+        lower_words = [word.lower() for word in all_words]
+        d = Counter(lower_words)
+        return d
 
 def seas_dept():
     with open("data/course1.csv", "rb") as input:
@@ -41,6 +42,12 @@ def pct_dict(diction):
         d[key] /= total_words
     return d
 
+def compare_dicts(d1, d2):
+    d = dict(d1)
+    for key in d:
+        d[key] -= d2[key]
+    return d
+
 def make_json(file_name, diction):
     d = [{"text":key, "size":value} for key,value in diction.items()]
     j = json.dumps(d)
@@ -50,7 +57,13 @@ def make_json(file_name, diction):
 seas = seas_dept()
 pct_seas = pct_dict(seas)
 
+test1 = export_test()
+pct_test = pct_dict(test1)
+
+test_final = compare_dicts(pct_test, pct_seas)
+
 make_json("data/seas.json", pct_seas)
+make_json("data/test_adjusted.json", test_final)
 
 
 
