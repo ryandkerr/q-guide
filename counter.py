19bed5c8999527500d600f5f53a77819f6371933
ryandkerr <ryankerr@college.harvard.edu>
2015-06-24 21:04:31 -0400

    added new output for seas dept, modified counter.py

--- counter.py
+++ counter.py
@@ -8,28 +8,48 @@ import re
 from collections import Counter
 
 # reading in test data csv and creating dictionary
-with open("data/test_q.csv", "rb") as test_csv:
-    test = csv.reader(test_csv)
-    for row in test:
-        if row[0] == "APCOMP 209: Data Science":
-            all_words = []
+def export_test():
+    with open("data/test_q.csv", "rb") as test_csv:
+        test = csv.reader(test_csv)
+        for row in test:
+            if row[0] == "APCOMP 209: Data Science":
+                all_words = []
+                for i, review in enumerate(row):
+                    words = re.findall(r'\w+', review)
+                    all_words += words
+                lower_words = [word.lower() for word in all_words]
+                c = Counter(lower_words)
+                
+                # creating json ouput 
+                d = [{"text":key, "size":value} for key,value in c.items()]
+                j = json.dumps(d, indent=4)
+                with open("data/test_export.json", "w") as test_export:
+                    print >> test_export, j
+
+def seas_dept():
+    with open("data/course1.csv", "rb") as input:
+        reader = csv.reader(input)
+        all_words = []
+        for row in reader:
             for i, review in enumerate(row):
                 words = re.findall(r'\w+', review)
                 all_words += words
-            lower_words = [word.lower() for word in all_words]
-            c = Counter(lower_words)
-            
-            # creating json ouput 
-            d = [{"text":key, "size":value} for key,value in c.items()]
-            j = json.dumps(d, indent=4)
-            with open("data/test_export.json", "w") as test_export:
-                print >> test_export, j
+        lower_words = [word.lower() for word in all_words]
+        c = Counter(lower_words)
 
-            # total_words = sum(c.itervalues())
+        # creating json output
+        d = [{"text":key, "size":value} for key,value in c.items()]
+        j = json.dumps(d)
+        with open("data/seas.json", "w") as seas_export:
+            print >> seas_export, j
 
-            # to change value for each dictionary item
-            # for key in c:
-            #     c[key] *= 2
+seas_dept()
+
+                # total_words = sum(c.itervalues())
+
+                # to change value for each dictionary item
+                # for key in c:
+                #     c[key] *= 2
 
 
 # further steps:
