d78247f4f2b8fa7ff4e180c3a9d3635dd06781c2
ryandkerr <ryankerr@college.harvard.edu>
2015-06-26 01:19:29 -0400

    first attempt at exporting all

--- counter.py
+++ counter.py
@@ -9,6 +9,31 @@ import re
 from collections import Counter
 
 # reading in test data csv and creating dictionary
+def count_row(row):
+    all_words = []
+    for review in row:
+        words = re.findall(r'\w+', review)
+        all_words += words
+    lower_words = [word.lower() for word in all_words]
+    d = Counter(lower_words)
+    return d
+
+def export_all(file_name):
+    global pct_seas
+    with open("data/course1.csv", "rb") as reader:
+        read = csv.reader(reader)
+        js_out = []
+        for row in read:
+            course_name = row[0]
+            d = compare_dicts(pct_dict(count_row(row)), pct_seas)
+            pruned = prune_dict(d, 40)
+            r = [{"text":key, "size":value} for key,value in pruned.items()]
+            j = [{"course": course_name, "reviews": r}]
+            js_out.append(j)
+        js = json.dumps(js_out)
+        with open(file_name, "w") as export:
+            print >> export, js
+
 def export_test():
     with open("data/course1.csv", "rb") as test_csv:
         test = csv.reader(test_csv)
@@ -33,16 +58,6 @@ def seas_dept():
         lower_words = [word.lower() for word in all_words]
         d = Counter(lower_words)
         return d
-
-def count_row(row):
-    all_words = []
-    for review in row:
-        words = re.findall(r'\w+', review)
-        all_words += words
-    lower_words = [word.lower() for word in all_words]
-    d = Counter(lower_words)
-    return d
-
 
 # functional-esque programming in python!
 def pct_dict(diction):
@@ -81,10 +96,11 @@ def make_json(file_name, diction):
 seas = seas_dept()
 pct_seas = pct_dict(seas)
 
-test1 = export_test()
-pct_test = pct_dict(test1)
+# test1 = export_test()
+# pct_test = pct_dict(test1)
 
-test_final = prune_dict(compare_dicts(pct_test, pct_seas), 40)
+# test_final = prune_dict(compare_dicts(pct_test, pct_seas), 40)
+export_all("data/all_classes.json")
 
 make_json("data/seas.json", pct_seas)
-make_json("data/test_adjusted.json", test_final)
+# make_json("data/test_adjusted.json", test_final)
