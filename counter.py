68053643c42a4399b15db1bed355da7f2169ad08
ryandkerr <ryankerr@college.harvard.edu>
2015-06-28 23:17:43 -0400

    added by dept data export json

--- counter.py
+++ counter.py
@@ -163,5 +163,5 @@ def make_json(file_name, diction):
 
 # make_json("data/seas.json", pct_seas)
 # make_json("data/test_adjusted.json", test_final)
-export_by_all("data/all_courses_all.json")
-
+# export_by_all("data/all_courses_all.json")
+export_by_dept("data/all_courses_dept.json")
